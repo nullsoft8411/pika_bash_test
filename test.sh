8229c14cde0d36922070bf815962703c96e98d33
nullsoft8411 <nullsoft8411@gmail.com>
2022-11-05 20:00:02 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-version="0.1"
-printf "test\n"
+version="0.2"
+getVer=$(curl https://raw.githubusercontent.com/nullsoft8411/pika_bash_test/main/version.txt)
+gitVersion=$(echo "$getVer" | awk -F : '{ print $2 }')
+if [[ $version < $gitVersion ]]; then
+printf "Update ist vorhanden: Aktuelle Version ${$version} verfügber ${gitVersion}"
+cp /home/nullsoft/pika_bash_test/test.sh old_test.sh
+rm  /home/nullosft/pika_bash_test/test.sh
+wget https://raw.githubusercontent.com/nullsoft8411/pika_bash_test/main/test.sh > /dev/null 2>&1
+printf "Script wurde aktulisiert auf $gitVer\n"
+else
+printf "Aktuelle Version: $version - Git Version $gitVersion\n"
+fi
