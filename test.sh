64ac93dbf974742583f42bc4e2fc527ec4879a03
Mustafa Sahin <nullsoft8411@gmail.com>
2022-11-07 20:34:09 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,12 +2,20 @@
 version="0.3"
 scriptPath=$(pwd)
 #curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/version.txt.sh/raw?ref=main > test.sh
-gitVersion=$(curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/version.txt.sh/raw?ref=main  | awk -F : '{ print $2 }')
+gitVersion=$(curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/version.txt/raw?ref=main  | awk -F : '{ print $2 }')
+forceUpdate=$(curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/version.txt/raw?ref=main  | awk -F = '{ print $2 }')
 if [[ $version < $gitVersion ]]; then
-if [[  ]]
-printf "Update ist vorhanden: Aktuelle Version ${$version} verfügber ${gitVersion}"
+if [[ $forceUpdate == "true" ]]; then
+printf "Das script wird auf $gitVersion geupdated"
 curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/test.sh/raw?ref=main > test.sh
 printf "Script wurde aktulisiert auf $gitVer\n"
+else
+printf "Ein neue version ist vorhanden. Script Update? [ja]"
+if [[ ${test:-} ]]; then
+ja
+fi
+
+fi
 else
 printf "Aktuelle Version: $version - Git Version $gitVersion\n"
 fi
