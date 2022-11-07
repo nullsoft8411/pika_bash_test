12c85cf0d6a6eb11607452008bac8a59f1777762
Mustafa Sahin <nullsoft8411@gmail.com>
2022-11-07 20:26:44 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 version="0.3"
 scriptPath=$(pwd)
-rm ${scriptPath}/version.txt || printf "es ist ein fehler aufgetreten"; exit 0;
-wget https://raw.githubusercontent.com/nullsoft8411/pika_bash_test/main/version.txt > /dev/null 2>&1
-gitVersion=$(awk -F : '{ print $2 }' $scriptPath/version.txt)
+#curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/version.txt.sh/raw?ref=main > test.sh
+gitVersion=$(curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/version.txt.sh/raw?ref=main  | awk -F : '{ print $2 }')
 if [[ $version < $gitVersion ]]; then
+if [[  ]]
 printf "Update ist vorhanden: Aktuelle Version ${$version} verfügber ${gitVersion}"
-cp ${scritPath}/test.sh $scriptPath/old_test.sh && rm  $scriptPath/test.sh
-wget https://raw.githubusercontent.com/nullsoft8411/pika_bash_test/main/test.sh > /dev/null 2>&1
+curl --header 'Private-Token: glpat-y3EsZ23s15tcuH_Xk7rT' https://gitlab.com/api/v4/projects/40850742/repository/files/test.sh/raw?ref=main > test.sh
 printf "Script wurde aktulisiert auf $gitVer\n"
 else
 printf "Aktuelle Version: $version - Git Version $gitVersion\n"
